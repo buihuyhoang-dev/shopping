e8d42986058a737874c27c52127c5f6bec3ab042
huyhoang <buihuyhoang.dev@gmail.com>
2024-04-16 22:05:11 +0700

    add new project

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.jeklama.mongodb.net',
+    DB_USER: 'buihuyhoangdev',
+    DB_PASS: '0386462080',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '<442>',
+    JWT_EXPIRES: '5454545454523', // in milliseconds
+    EMAIL_USER: '<buihuyhoang.10k11@gmail.com>', // gmail service
+    EMAIL_PASS: '<bljdoaiuthwrjpde>'
+};
+module.exports = MyConstants;
